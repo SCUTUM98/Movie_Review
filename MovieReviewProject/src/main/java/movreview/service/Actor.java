ac3ab441c969d3f8476b1ea968fd15f53525b441
SCUTUM98 <SCUTUM98@DESKTOP-GOMQG76>
2024-10-11 18:25:52 +0900

    2024.10.11 수정사항

---
+++ MovieReviewProject/src/main/java/movreview/service/Actor.java
@@ -0,0 +1,11 @@
+package movreview.service;
+
+public class Actor {
+	private int movieId;
+	private String characterId;
+	private int gender;
+	private String actName;
+	private String originalName;
+	private String profilePath;
+	private String character;
+}
---
+++ MovieReviewProject/src/main/java/movreview/service/Collection.java
@@ -0,0 +1,10 @@
+package movreview.service;
+
+public class Collection {
+	private int id;
+	private String name;
+	private String posterPath;
+	private String backdropPath;
+	
+	
+}
