a2edb244dd3ce0613a6ee4ea975438c171853d58
SCUTUM98 <admin@DESKTOP-GOMQG76>
2024-10-11 15:37:37 +0900

    set MovieVO

--- MovieReviewProject/src/main/java/movreview/service/MovieVO.java
+++ MovieReviewProject/src/main/java/movreview/service/MovieVO.java
@@ -1,5 +1,93 @@
 package movreview.service;
 
 public class MovieVO {
-
+	private int movieId;
+	private String titleKr;
+	private String titleEn;
+	private String genre;
+	private String releaseDate;
+	private String overview;
+	private String backdropPath;
+	private String posterPath;
+	private int collectionId;
+	private String status;
+	private String tagline;
+	
+	public int getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+	
+	public String getTitleKr() {
+		return titleKr;
+	}
+	public void setTitleKr(String titleKr) {
+		this.titleKr = titleKr;
+	}
+	
+	public String getTitleEn() {
+		return titleEn;
+	}
+	public void setTitleEn(String titleEn) {
+		this.titleEn = titleEn;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	
+	public String getOverview() {
+		return overview;
+	}
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+	
+	public String getBackdropPath() {
+		return backdropPath;
+	}
+	public void setBackdropPath(String backdropPath) {
+		this.backdropPath = backdropPath;
+	}
+	
+	public String getPosterPath() {
+		return posterPath;
+	}
+	public void setPosterPath(String posterPath) {
+		this.posterPath = posterPath;
+	}
+	
+	public int getCollectionId() {
+		return collectionId;
+	}
+	public void setCollectionId(int collectionId) {
+		this.collectionId = collectionId;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public String getTagline() {
+		return tagline;
+	}
+	public void setTagline(String tagline) {
+		this.tagline =  tagline;
+	}
+	
 }
