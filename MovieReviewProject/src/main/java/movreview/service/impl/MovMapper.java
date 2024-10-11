1f5b09566c28fb4ae6df8db32bdc79078a164e41
SCUTUM98 <SCUTUM98@DESKTOP-GOMQG76>
2024-10-11 13:35:02 +0900

    add mapper

---
+++ MovieReviewProject/src/main/java/movreview/service/impl/MovMapper.java
@@ -0,0 +1,10 @@
+package movreview.service.impl;
+
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+
+import movreview.service.MovieVO;
+
+@Mapper("movMapper")
+public interface MovMapper {
+
+}
