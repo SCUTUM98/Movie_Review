c06dd3e53087b6b98349747420e84a1730abeb8e
SCUTUM98 <SCUTUM98@DESKTOP-GOMQG76>
2024-10-14 13:28:58 +0900

    update vo

--- MovieReviewProject/src/main/java/movreview/service/CollectionVO.java
+++ MovieReviewProject/src/main/java/movreview/service/CollectionVO.java
@@ -5,4 +5,32 @@ public class CollectionVO {
 	private String name;
 	private String posterPath;
 	private String backdropPath;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPosterPath() {
+		return posterPath;
+	}
+	public void setPosterPath(String posterPath) {
+		this.posterPath = posterPath;
+	}
+	
+	public String getBackdropPath() {
+		return backdropPath;
+	}
+	public void setBackdropPath(String backdropPath) {
+		this.backdropPath = backdropPath;
+	}
 }
